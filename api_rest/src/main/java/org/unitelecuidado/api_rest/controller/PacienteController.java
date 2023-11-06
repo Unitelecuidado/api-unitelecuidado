ebcc10c81365a71e4726e1e9fec30ab292226af2
Victor <victoroctavio4@gmail.com>
2023-11-06 20:57:02 -0300

    SCRUM-43 feat: Lista Paciente por ID

--- api_rest/src/main/java/org/unitelecuidado/api_rest/controller/PacienteController.java
+++ api_rest/src/main/java/org/unitelecuidado/api_rest/controller/PacienteController.java
@@ -4,12 +4,14 @@ package org.unitelecuidado.api_rest.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.unitelecuidado.api_rest.domain.paciente.Paciente;
 import org.unitelecuidado.api_rest.dto.paciente.PacienteAtualizar;
 import org.unitelecuidado.api_rest.dto.paciente.PacienteCadastro;
 import org.unitelecuidado.api_rest.repository.PacienteRepository;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -28,6 +30,19 @@ public class PacienteController {
     public List<Paciente> lista(){
         return repository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Paciente> listaID(@PathVariable Long id) {
+        Optional<Paciente> pacienteOptional = repository.findById(id);
+        if (pacienteOptional.isPresent()) {
+            Paciente paciente = pacienteOptional.get();
+            return ResponseEntity.ok(paciente);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
 
     @PutMapping
     @Transactional
